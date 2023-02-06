e7688711cc31f8090ffcba31d7ad56470226902f
ZinebSaidi <ZinebSaidi@DESKTOP-FL026QA>
2023-02-06 17:01:00 +0100

    Ajoutez des fichiers projet.

---
+++ SINEID/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SINEID
+{
+    [Activity(Label = "SplashScreen")]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.SplashScreen);
+            // Create your application here
+        }
+    }
+}
